de477cfb8471e40b8faab78c21ae8b4cd82c3f75
TryfanEvans <TryfanLE@gmail.com>
2020-07-10 12:11:59 +0930

    Proofread and Font

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
 import "./contact.css"
+import Button from '../Components/Button'
+
 export default class Contact extends Component {
+    state = {
+        email: "",
+        message: ""
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        
+    }
+
+    onChange = e => {
+        this.setState({[e.target]: [e.value]})
+    }
     render() {
+        console.log(this.state.email)
         return (
             <div className = "Contact">
-               <h1></h1>
+               <h1> Contact </h1>
+               <form>
+                   <input placeholder = "email" />
+                   <textarea cols = "50" placeholder = "send us a message" />
+                   <Button style = {{margin: 0}} className = "button" type = "submit" placeholder = "submit" name = "Submit" />
+               </form>
             </div>
         )
     }
