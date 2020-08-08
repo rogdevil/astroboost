6cefdc56e3e2b1d7bad75d26557f1c6168634751
sep2001aryan <sep2001aryan@yahoo.com>
2020-08-08 05:28:16 -0700

    mobile screen optimization

--- public/controller.js
+++ public/controller.js
@@ -15,31 +15,6 @@ $(document).ready(() => {
     console.log("scrolling");
   });
 
-  var $animation_elements = $(".show-on-scroll");
-  var $window = $(window);
-  function check_if_in_view() {
-    var window_height = $window.height();
-    var window_top_position = $window.scrollTop();
-    var window_bottom_position = window_top_position + window_height;
-
-    $(".show-on-scroll").each(function () {
-      var $element = $(this);
-      var element_height = $element.outerHeight();
-      var element_top_position = $element.offset().top;
-      var element_bottom_position = element_top_position + element_height;
-
-      //check to see if this current container is within viewport
-      if (
-        element_bottom_position >= window_top_position &&
-        element_top_position < window_bottom_position
-      ) {
-        $element.addClass("in-view");
-      } else {
-        $element.removeClass("in-view");
-      }
-    });
-  }
-
-  $window.on("scroll", check_if_in_view);
-  $window.trigger("scroll");
+  //scroll animation
+  
 });
